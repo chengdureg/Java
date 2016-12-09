abfde715a98db898a7a2a471660ea8b61ccf5138
chengdureg <chengdu.reg@qq.com>
2016-12-09 13:56:59 +0800

    Initialize the Java projects.

---
+++ src/com/Ni/Mike/Main.java
@@ -0,0 +1,9 @@
+package com.Ni.Mike;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Just for study only source code");
+    }
+}
